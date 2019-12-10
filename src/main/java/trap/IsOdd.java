5955dafb747706d8c9f6035768b373175058a821
anfeel <anfeel@foxmail.com>
2019-12-10 11:08:50 +0800

    提交【判断奇数】

---
+++ src/main/java/trap/IsOdd.java
@@ -0,0 +1,39 @@
+package trap;
+
+import org.junit.Test;
+
+/**
+ * 奇数性
+ * @author anfeel
+ * @version $Id: IsOdd.java, v 0.1 2019年12月10日 上午11:00:30 anfeel Exp $
+ */
+public class IsOdd {
+
+    public static boolean isOdd(int i) {
+        return (i & 1) != 0;
+        //        return i % 2 != 0;
+        //        return i % 2 == -1 || i % 2 == 1;
+        //        return i % 2 == 1;
+    }
+
+    @Test
+    public void test1() {
+        System.out.println(isOdd(1));
+    }
+
+    @Test
+    public void test2() {
+        System.out.println(isOdd(2));
+    }
+
+    @Test
+    public void test3() {
+        System.out.println(isOdd(-3));
+    }
+
+    @Test
+    public void test4() {
+        System.out.println(isOdd(0));
+    }
+
+}
