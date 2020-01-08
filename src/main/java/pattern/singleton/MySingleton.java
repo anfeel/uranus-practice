28f1f6442e3535150b10ba9dc017b7faffa20e4f
anfeel <anfeel@foxmail.com>
2020-01-08 11:39:44 +0800

    commit 【Singleton Pattern】 , add simple solution

---
+++ src/main/java/pattern/singleton/MySingleton.java
@@ -0,0 +1,37 @@
+package pattern.singleton;
+
+import org.apache.commons.lang3.RandomUtils;
+
+/**
+ * 单例模式
+ * 1. 私有构造方法，防止外部类调用
+ * 2. 私有静态对象实例，实例只会在类初始化后进行构造
+ * 3. 公有静态方法获取私有实例
+ * @author anfeel
+ * @version $Id: MySingleton.java, v 0.1 2020年1月7日 下午2:43:30 anfeel Exp $
+ */
+public class MySingleton {
+
+    private int                num;
+
+    private static MySingleton mySingleton = new MySingleton();
+
+    private MySingleton() {
+        num = RandomUtils.nextInt();
+    }
+
+    public int getNum() {
+        return this.num;
+    }
+
+    public static MySingleton getInstance() {
+        return mySingleton;
+    }
+
+    public static void main(String[] args) {
+        MySingleton mySingleton = MySingleton.getInstance();
+        for (int i = 0; i <= 4; i++) {
+            System.out.println(mySingleton.getNum());
+        }
+    }
+}
