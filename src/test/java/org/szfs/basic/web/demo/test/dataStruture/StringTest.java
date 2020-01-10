319f7de91f7005c69bf807a195fead8a385e5fd9
anfeel <anfeel@foxmail.com>
2020-01-10 15:39:36 +0800

    commit 【StringTest】 and   test the indent size in IDEA

---
+++ src/test/java/org/szfs/basic/web/demo/test/dataStruture/StringTest.java
@@ -0,0 +1,15 @@
+package org.szfs.basic.web.demo.test.dataStruture;
+
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Test;
+
+/**
+ * @author anfeel
+ * @version org.szfs.basic.web.demo.test.dataStruture, v 0.1 2020/1/10 15:35 anfeel Exp $$
+ */
+public class StringTest {
+        @Test
+        public void test1() {
+                System.out.println(StringUtils.substring("ABCDEFG", 1, 3));
+        }
+}
