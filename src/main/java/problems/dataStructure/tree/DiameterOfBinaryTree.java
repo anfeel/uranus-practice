78ee1014bff3fce86ad305ce82d4d8348846458f
anfeel <anfeel@foxmail.com>
2020-01-19 11:24:51 +0800

    update 【Diameter of Binary Tree】, add test case

--- src/main/java/problems/dataStructure/tree/DiameterOfBinaryTree.java
+++ src/main/java/problems/dataStructure/tree/DiameterOfBinaryTree.java
@@ -94,4 +94,44 @@ public class DiameterOfBinaryTree {
     public void test5() {
         Assert.assertEquals(0, diameterOfBinaryTree(null));
     }
+
+    @Test
+    public void test6() {
+        TreeNode t1 = new TreeNode(4);
+        TreeNode t2 = new TreeNode(-7);
+        TreeNode t3 = new TreeNode(-3);
+        TreeNode t4 = new TreeNode(-9);
+        TreeNode t5 = new TreeNode(-3);
+        TreeNode t6 = new TreeNode(9);
+        TreeNode t7 = new TreeNode(-7);
+        TreeNode t8 = new TreeNode(-4);
+        TreeNode t9 = new TreeNode(6);
+        TreeNode t10 = new TreeNode(-6);
+        TreeNode t11 = new TreeNode(-6);
+        TreeNode t12 = new TreeNode(0);
+        TreeNode t13 = new TreeNode(6);
+        TreeNode t14 = new TreeNode(5);
+        TreeNode t15 = new TreeNode(9);
+        TreeNode t16 = new TreeNode(-1);
+        TreeNode t17 = new TreeNode(-4);
+        TreeNode t18 = new TreeNode(-2);
+        t1.left = t2;
+        t1.right = t3;
+        t3.left = t4;
+        t3.right = t5;
+        t4.left = t6;
+        t4.right = t7;
+        t5.left = t8;
+        t6.left = t9;
+        t6.right = t10;
+        t7.right = t11;
+        t9.left = t12;
+        t9.right = t13;
+        t10.left = t14;
+        t11.left = t15;
+        t12.right = t16;
+        t13.right = t17;
+        t15.left = t18;
+        Assert.assertEquals(8, diameterOfBinaryTree(t1));
+    }
 }
