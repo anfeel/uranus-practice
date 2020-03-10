623318fc9e7462b6d5e982d4bf64d81ecd5b4e3e
anfeel <anfeel@foxmail.com>
2020-03-10 22:38:33 +0800

    commit 【bean compare test】

--- src/test/java/org/szfs/basic/web/demo/test/java/BeanCopyTest.java
+++ src/test/java/org/szfs/basic/web/demo/test/java/BeanCopyTest.java
@@ -1,8 +1,7 @@
 package org.szfs.basic.web.demo.test.java;
 
-import org.apache.commons.beanutils.BeanUtils;
-
 import java.lang.reflect.InvocationTargetException;
+import java.util.List;
 
 /**
  *
@@ -11,13 +10,29 @@ import java.lang.reflect.InvocationTargetException;
  */
 public class BeanCopyTest {
 
+    public void getTest(test1 test1, test2 test2) {
+        test1.setAaa("123");
+        test1.setAab(2);
+        saveTest(test1, test2);
+    }
+
+    public void saveTest(test1 test1, test2 test2) {
+        test2.setAaa("312");
+        test2.setAab(3);
+    }
+
+    private void saveList(List<String> list) {
+        list.add("123");
+        list.add("111");
+        list.add("223");
+    }
+
     public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
         test1 test1 = new test1();
-        test1.setAaa("123");
-        test1.setAab(1);
         test2 test2 = new test2();
-        BeanUtils.copyProperties(test2, test1);
-        System.out.println(" aaa : " + test2.getAaa());
-        System.out.println(" aab : " + test2.getAab());
+        BeanCopyTest test = new BeanCopyTest();
+        test.getTest(test1, test2);
+        System.out.println(test1.getAaa());
+        System.out.println(test2.getAaa());
     }
 }
