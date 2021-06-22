7c6f722e14b462fa2919e967799964924690d679
anfeel <anfeel@foxmail.com>
2021-06-22 11:26:27 +0800

    commit 【ReadWriteLockTest】

---
+++ src/main/java/concurrency/lock/ReadWriteLockTest.java
@@ -0,0 +1,59 @@
+package concurrency.lock;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ *
+ * @author anfeel
+ * @version $ Id:ReadWriteLockTest, v 0.1 2021年06月22日 11:04 anfeel Exp $
+ */
+public class ReadWriteLockTest {
+
+    ReadWriteLock rwl = new ReentrantReadWriteLock();
+
+    public void read(Thread thread) {
+        rwl.readLock().lock();
+        try {
+            for (int i = 0; i < 10; i++) {
+                System.out.println("thread : " + thread.getName() + " , reading ");
+            }
+            System.out.println("thread : " + thread.getName() + " , reading finish");
+        } finally {
+            rwl.readLock().unlock();
+        }
+    }
+
+    public void write(Thread thread) {
+        rwl.writeLock().lock();
+        try {
+            for (int i = 0; i < 10; i++) {
+                System.out.println("thread : " + thread.getName() + " , writing ");
+            }
+            System.out.println("thread : " + thread.getName() + " , writing finish");
+        } finally {
+            rwl.writeLock().unlock();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        ReadWriteLockTest test = new ReadWriteLockTest();
+        ExecutorService pool = new ThreadPoolExecutor(4, 4, 0L, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>());
+        for (int i = 0; i < 4; i++) {
+            pool.execute(() -> {
+                test.read(Thread.currentThread());
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                test.write(Thread.currentThread());
+            });
+        }
+    }
+}
